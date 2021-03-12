03c0bf1d641872c11b1dd781fabcb5131246ff75
Dendrobyte <markbacon78@gmail.com>
2021-03-12 13:45:16 -0500

    Added bootstrap to dependencies, navbar component completed (awaiting hamburger)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
+import Navbar from './Navbar.js'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <Navbar signedIn="true" />
+
+        <p>Page content starts here! There's padding within the navbar which is why this looks off.</p>
     </div>
   );
 }
