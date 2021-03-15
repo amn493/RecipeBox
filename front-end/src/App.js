0f736b4fc179117d311d3febb4485825d6fb6357
Jonathan Susman <jas1611@nyu.edu>
2021-03-14 21:18:59 -0400

    Revert "resolving package conflicts"
    This reverts commit bc74af46d6e3bbb4cad0673adce601ea822df879.
    
    reverting a commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,23 @@
-import React from 'react'
-
-import HamburgerNotSignedIn from './HamburgerNotSignedIn.js'
+import logo from './logo.svg';
 import './App.css';
-
 
 function App() {
   return (
-    <div className='App' id='outer-container'>
-      {/*<HamburgerNotSignedIn pageWrapId={ 'page-wrap' } outerContainerId={ 'outer-container' } />*/}
-      <main id='page-wrap'>
-        <p>Page content starts here! There's padding within the navbar which is why this looks off.</p>
-      </main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
