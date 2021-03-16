3052778ceb423464f8357b2f28a60ee24f648a5b
Ashley Nussbaum <amn493@nyu.edu>
2021-03-16 14:17:13 -0400

    Adding signed-in user to props and determining if recipe was liked for small recipe preview

--- front-end/src/SmallRecipePreview.js
+++ front-end/src/SmallRecipePreview.js
@@ -2,22 +2,25 @@ import './SmallRecipePreview.css'
 
 //Component for small recipe previews
 //Expects a recipe object as props
-//Example: <SmallRecipePreview recipe={{imagePath: 'logo192.png', slug: 'foobarguacamole', name: 'Guacamole', user: {username: 'foobar'}, likes: 36}} liked={false}></SmallRecipePreview>
+//Example: <SmallRecipePreview recipe={{imagePath: 'logo192.png', slug: 'foobar-guacamole', name: 'Guacamole', user: {username: 'foobar'}, likes: 36, id: 2, createdAt: 1615864425952}} user={{liked: [1, 2, 3, 4, 5]}}></SmallRecipePreview>
 const SmallRecipePreview = (props) => {
+    
+    const liked = props.user.liked.includes(props.recipe.id)
+    
     return (
         <table className="smallRecipePreview">
             <tr>
-            <td>
-                <img className="smallRecipePreviewImage" src={props.recipe.imagePath} alt="food"></img>
-            </td>
-            <td>
-                <a className="smallRecipePreviewRecipeName" href={'/recipe/' + props.recipe.slug}>{props.recipe.name}</a>
-                <img className="heartImage" src={props.liked ? 'heartFill.png' : 'heartOutline.png'} alt={props.liked ? 'heart fill' : 'heart outline'}></img>
-                {props.recipe.likes}
-                <br></br>
-                <a className="smallRecipePreviewUsername" href={'/user/' + props.recipe.user.username}>{'@' + props.recipe.user.username}</a>
-                {props.recipe.createdAt}
-            </td>
+                <td>
+                    <img className="smallRecipePreviewImage" src={props.recipe.imagePath} alt="food" />
+                </td>
+                <td>
+                    <a className="smallRecipePreviewRecipeName" href={'/recipe/' + props.recipe.slug}>{props.recipe.name}</a>
+                    <img className="heartImage" src={liked ? 'heartFill.png' : 'heartOutline.png'} alt={liked ? 'heart fill' : 'heart outline'} />
+                    {props.recipe.likes}
+                    <br />
+                    <a className="smallRecipePreviewUsername" href={'/user/' + props.recipe.user.username}>{'@' + props.recipe.user.username}</a>
+                    {props.recipe.createdAt}
+                </td>
             </tr>
         </table>
     )
