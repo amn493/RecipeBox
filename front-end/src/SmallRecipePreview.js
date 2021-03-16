68123d018c8f7ab0ac9d6debc092e34eb504873a
Ashley Nussbaum <amn493@nyu.edu>
2021-03-16 15:31:38 -0400

    Fixing link href slugs for small recipe preview

--- front-end/src/SmallRecipePreview.js
+++ front-end/src/SmallRecipePreview.js
@@ -4,7 +4,7 @@ import './SmallRecipePreview.css'
 
 //Component for small recipe previews
 //Expects a recipe object as props
-//Example: <SmallRecipePreview recipe={{imagePath: 'logo192.png', slug: 'foobar-guacamole', name: 'Guacamole', user: {username: 'foobar'}, likes: 36, id: 2, createdAt: 1615864425952}} user={{liked: [1, 2, 3, 4, 5]}}></SmallRecipePreview>
+//Example: <SmallRecipePreview recipe={{imagePath: 'logo192.png', slug: 'foobar-guacamole', name: 'Guacamole', user: {username: 'foobar', slug: 'foobar'}, likes: 36, id: 2, createdAt: 1615864425952}} user={{liked: [1, 2, 3, 4, 5]}}></SmallRecipePreview>
 const SmallRecipePreview = (props) => {
     
     const liked = props.user.liked.includes(props.recipe.id)
@@ -16,11 +16,11 @@ const SmallRecipePreview = (props) => {
                     <img className="smallRecipePreviewImage" src={props.recipe.imagePath} alt="food" />
                 </td>
                 <td>
-                    <a className="smallRecipePreviewRecipeName" href={'/recipe/' + props.recipe.slug}>{props.recipe.name}</a>
+                    <a className="smallRecipePreviewRecipeName" href={'/recipe-' + props.recipe.slug}>{props.recipe.name}</a>
                     <img className="heartImage" src={liked ? 'heartFill.png' : 'heartOutline.png'} alt={liked ? 'heart fill' : 'heart outline'} />
                     {props.recipe.likes}
                     <br />
-                    <a className="smallRecipePreviewUsername" href={'/user/' + props.recipe.user.username}>{'@' + props.recipe.user.username}</a>
+                    <a className="smallRecipePreviewUsername" href={'/user-' + props.recipe.user.slug}>{'@' + props.recipe.user.username}</a>
                     <Timestamp createdAt={props.recipe.createdAt} />
                 </td>
             </tr>
