bbd71b8c89cf7d52c3f70b360d5b29f97b7188b2
Dendrobyte <markbacon78@gmail.com>
2021-03-16 20:52:24 -0400

    Setting up recipelist component for recipebox and feedpage

---
+++ front-end/src/RecipeList.js
@@ -0,0 +1,42 @@
+import LargeRecipePreview from './LargeRecipePreview.js'
+import SmallRecipePreview from './SmallRecipePreview.js'
+import './RecipeList.css'
+
+// Generates a list of recipes with a particular amount passed in
+// props.size -- "small" or "large" depending on preview size
+// props.recipes -- The passed in database list of recipes
+// props.user -- This is the user object of the user signed in
+const RecipeList = (props) => {
+    
+    // Will put "recipe" and "likes" details here when passed in / fetched from database
+    let previewArray = []
+    let preview
+
+    props.recipes.map((recipeEntry, index) => {
+            // Define whether or not the previews are large and small (default to small)
+            if (props.size === "large"){
+                // TODO: Put in the recipe to be "entry" and get rid of likes
+                preview = <LargeRecipePreview key={index} recipe={recipeEntry} user={props.user}/>
+            } else {
+                preview = <SmallRecipePreview key={index} recipe={recipeEntry} user={props.user}/>
+            }
+
+            // Add the proper list to the previewarray
+            previewArray.push(
+                <div className="entry-single">
+                    {preview}
+                </div>
+            )
+        }
+    )
+    
+    // Return the list of previews
+    return (
+        <div className=".entry-body container">
+            {previewArray}
+        </div>
+    )
+
+}
+
+export default RecipeList
