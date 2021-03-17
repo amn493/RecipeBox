3128d7e5ddd7d6cbbc46ce99896835f8b52cf740
olivia edell <oliviaedell@olivias-mbp.myfiosgateway.com>
2021-03-16 23:05:34 -0400

    add css for Follow/Unfollow button

--- front-end/src/FollowButton.js
+++ front-end/src/FollowButton.js
@@ -1,41 +1,38 @@
 import React, { useState } from "react"
 import "./FollowButton.css"
 
-
 const FollowButton = (props) => {
-
+  let state = "Follow"
+  if (props.profile in props.user.following) {
+    state = "Unfollow"
+  }
 /* text is a state variable that changes the text
 on the button depending on whether the active user 
-is already following the profile they are viewing */  
-    let state = "Follow"
-    if (props.profile in props.user.following) {
-        state = "Unfollow"
-    }
-    let [text, setText] = useState(state)
+is already following the profile they are viewing */
+  let [text, setText] = useState(state)
 
 /* click event handler changes text state 
     and css styles for the button */
-    function follow(evt) {
-        setText((prevText) => {
-            if (prevText === "Follow") {
-                evt.target.classList.add("following")
-                evt.target.classList.remove("not-following")
-                return "Unfollow"
-            } else {
-                evt.target.classList.add("not-following")
-                evt.target.classList.remove("following")
-                return "Follow"
-            }
-        })
-    }
+  function follow(evt) {
+    setText((prevText) => {
+      if (prevText === "Follow") {
+        evt.target.classList.add("follow-btn-following")
+        evt.target.classList.remove("follow-btn-not-following")
+        return "Unfollow"
+      } else {
+        evt.target.classList.add("follow-btn-not-following")
+        evt.target.classList.remove("follow-btn-following")
+        return "Follow"
+      }
+    })
+  }
 
 /* component */
-    return (
-        <button className="FollowBtn" onClick={follow}>
-            {text}
-        </button>
-    )
-
-}
+  return (
+    <button className="follow-btn" onClick={follow}>
+      {text}
+    </button>
+  );
+};
 
 export default FollowButton
