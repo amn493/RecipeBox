2385e7004a059c13d4170b1595eb4ad0237cf895
anvitayella <ay1313@nyu.edu>
2021-03-19 10:55:54 -0400

    Changed classnames to camelCase

--- front-end/src/HamburgerNotSignedIn.js
+++ front-end/src/HamburgerNotSignedIn.js
@@ -10,10 +10,10 @@ const HamburgerNotSignedIn = (props) => {
     return (
         <div>
         <Menu>
-            <a className='menu-item' href='/signin'>Sign In</a>
-            <a className='menu-item' href='/createaccount'>Create Account</a>
-            <a className='menu-item' href='/browserecipes'>Browse Recipes</a>
-            <a className='menu-item' href='/browseusers'>Browse Users</a>
+            <a className='menuItem' href='/signin'>Sign In</a>
+            <a className='menuItem' href='/createaccount'>Create Account</a>
+            <a className='menuItem' href='/browserecipes'>Browse Recipes</a>
+            <a className='menuItem' href='/browseusers'>Browse Users</a>
         </Menu>
         </div>
     )
