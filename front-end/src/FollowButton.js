0bb37187700eb62c7d39eebb4f54c0b855ec77a8
Ashley Nussbaum <amn493@nyu.edu>
2021-03-20 16:41:40 -0400

    Display sign-in modal if a not signed-in user attemps to follow someone

--- front-end/src/FollowButton.js
+++ front-end/src/FollowButton.js
@@ -6,21 +6,30 @@ const FollowButton = (props) => {
   if (props.profileUserId in props.currentUser.following) {
     state = "Following"
   }
-/* text is a state variable that changesg the text
-on the button depending on whether the active user 
-is already following the profile they are viewing */
+  /* text is a state variable that changes the text
+  on the button depending on whether the active user 
+  is already following the profile they are viewing */
   let [text, setText] = useState(state)
 
-/* click event handler changes text state 
-    and css styles for the button */
-  function follow(evt) {
-    setText((prevText) => {
-      if (prevText === "Follow") {
-        return "Following"
-      } else {
-        return "Follow"
-      }
-    })
+  // click event handler changes text state 
+  function follow() {
+
+    if (props.signedIn) {
+      setText((prevText) => {
+        if (prevText === "Follow") {
+          return "Following"
+        } else {
+          return "Follow"
+        }
+      })
+  
+      // TODO: update props.currentUser.following and props.profileUserId's user .following in the database
+    }
+    else {
+      // show sign-in modal if a not-signed in user attempts to follow a user
+      props.setShowModal(true)
+    }
+    
   }
 
 /* component */
