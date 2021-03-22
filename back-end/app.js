7102b1bd49429bc034c4c67da57238fe46815f1e
Ashley Nussbaum <amn493@nyu.edu>
2021-03-22 18:01:11 -0400

    Add dependency for multer

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,16 @@
 // import and instantiate express
 const express = require("express") // CommonJS import style!
-const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
 const app = express() // instantiate an Express object
+
+const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
+const multer = require("multer") // middleware to handle HTTP POST requests with file uploads
+
+
 app.use(morgan("dev")) // dev style gives a concise color-coded style of log output
+
+
 // we will put some server logic here later...
+
+
 // export the express app we created to make it available to other modules
 module.exports = app
