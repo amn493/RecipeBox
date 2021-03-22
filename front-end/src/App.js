02e79c2be5b383892d2ca97c345f0684a403fcc1
Ashley Nussbaum <amn493@nyu.edu>
2021-03-21 23:58:32 -0400

    Add user to props to fix bug

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -82,7 +82,7 @@ function App() {
 
             {/* BROWSE RECIPES PAGE */}
             <Route path="/browse-recipes">
-              <BrowseRecipesPage />
+              <BrowseRecipesPage user={user} />
             </Route>
 
             {/* RECIPE PAGE */}
