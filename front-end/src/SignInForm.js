1edba081144dea7ec2f0aca30d01f94d39956f31
anvitayella <ay1313@nyu.edu>
2021-03-21 22:13:25 -0400

    Edited button styling (info and block)

--- front-end/src/SignInForm.js
+++ front-end/src/SignInForm.js
@@ -42,7 +42,7 @@ const SignInForm = (props) =>  {
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
-        <Button className='submitButton' type='submit' variant='secondary' /*disabled={!validateForm()}*/>
+        <Button className='submitButton' type='submit' variant='info' block /*disabled={!validateForm()}*/>
           Sign In
         </Button>
       </Form>
