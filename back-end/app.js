58ce62a0ccab01fcda18dd2eab63edad9920bc61
Ashley Nussbaum <amn493@nyu.edu>
2021-03-22 17:55:14 -0400

    Add dependencies for nodemon and morgan

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,8 @@
 // import and instantiate express
 const express = require("express") // CommonJS import style!
+const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
 const app = express() // instantiate an Express object
+app.use(morgan("dev")) // dev style gives a concise color-coded style of log output
 // we will put some server logic here later...
 // export the express app we created to make it available to other modules
 module.exports = app
