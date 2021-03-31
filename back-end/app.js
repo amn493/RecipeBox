c2a9119bd323da02b1037ea151dc807b71656d1e
Ashley Nussbaum <amn493@nyu.edu>
2021-03-30 22:10:42 -0400

    Implement route handler for GET /userbyid

--- back-end/app.js
+++ back-end/app.js
@@ -62,6 +62,16 @@ app.get('/usersbyid', (req, res) => {
     .catch(err => next(err))
 })
 
+app.get('/userbyid', (req, res) => {
+
+    // fetch user where _id === req.query.id from database
+
+    axios
+    .get('https://my.api.mockaroo.com/user.json?key=f6a27260')
+    .then(apiResponse => res.json(apiResponse.data[0]))
+    .catch(err => next(err))
+})
+
 app.get('/comments', (req, res) => {
 
     // fetch comments where recipe === req.query.recipeID from database
