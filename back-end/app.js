aecdcf31ac1389afa45653bda349b5e86a14505f
Jonathan Sussman <60412241+JonathanSussman1@users.noreply.github.com>
2021-03-31 14:04:54 -0400

    Update app.js
    changed structure of /blockuser POST request route handler

--- back-end/app.js
+++ back-end/app.js
@@ -120,14 +120,20 @@ app.post('/comment', (req, res) => {
 
 app.post('/blockuser', (req, res) => {
 
-    // block or unblock a user
+    // update signed-in user's blockedUsers array appropriately
 
-    const data = {
-        id: req.body.id,
-        addBlock: req.body.addBlock,
+    const updatedBlockedUsers = req.body.blockedUsers
+    if(req.body.addBlock) {
+        updatedBlockedUsers.push(req.body.id)
     }
-    res.json(data)
+    else {
+        updatedBlockedUsers.splice(updatedBlockedUsers.indexOf(req.body.id), 1)
+    }
+    
+    res.json(updatedBlockedUsers)
+
 })
+
 
 
 
