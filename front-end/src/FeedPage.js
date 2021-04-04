53e060331bbd23048c3e42cc1573ddbe21571313
Mark <markbacon78@gmail.com>
2021-04-04 16:14:26 -0400

    Error component base done

--- front-end/src/FeedPage.js
+++ front-end/src/FeedPage.js
@@ -3,6 +3,7 @@ import RecipeList from './RecipeList'
 
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import ErrorComponent from './ErrorComponent'
 
 // Pulls recipes from the database for the logged-in feed and generates a recipelist for qualifying recipes
 // Qualifying recipes, e.g. latest recipes posted by those someone has followed
@@ -20,16 +21,22 @@ const Feed = (props) => {
         axios('https://my.api.mockaroo.com/recipe.json?key=f6a27260').then((response) => {
             setRecBoxRecipes(response.data)
         }).catch((err) =>{
-            // TODO: Print an error to the user, but for now mockaroo is likely
+            // TODO: set rec box recipes to the ErrorComponent
             console.log(err)
             setRecBoxRecipes(feedRecipes)
         })
     }, [])
 
     return (
+        <>
+
         <div className="container">
             <RecipeList size="large" recipes={recBoxRecipes} user={props.user} />
         </div>
+
+        </>
+
+        
     )
 }
 
