e3f1edde231f434680756c3a21b7922bed08f35b
anvitayella <ay1313@nyu.edu>
2021-04-04 12:05:30 -0400

    Form validation for create account

--- front-end/src/CreateAccountPage.js
+++ front-end/src/CreateAccountPage.js
@@ -7,149 +7,174 @@ import './CreateAccountPage.css'
 // Does not expect any argument for props
 // Example: 
 // <CreateAccount />
-const CreateAccountPage = (props) =>  {
+const CreateAccountPage = () =>  {
+  const [email, setEmail] = useState('')
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [ReEnterPassword, setReEnterPassword] = useState('')
-  const [email, setEmail] = useState('')
-  const [error, setError] = useState(false)
-  const [errorMessages, setErrorMessages] = useState([])
+
+  const [initialEmail, setInitialEmail] = useState(true)
+  const [initialFirstName, setInitialFirstName] = useState(true)
+  const [initialLastName, setInitialLastName] = useState(true)
+  const [initialUsername, setInitialUsername] = useState(true)
+  const [initialPassword, setInitialPassword] = useState(true)
+  const [initialReEnterPassword, setInitialReEnterPassword] = useState(true)
 
   function handleSubmit(event) {
-    event.preventDefault()
-    setError(false)
-    setErrorMessages([])
-    validateForm()
-  }
-
-  function emptyField() {
-    if (email.length === 0) {
-      return true
-    }
-    if (firstName.length === 0) {
-      return true
-    }
-    if (lastName.length === 0) {
-      return true
-    }
-    if (username.length === 0) {
-      return true
-    }
-    if (password.length === 0) {
-      return true
-    }
-    if (ReEnterPassword.length === 0) {
-      return true
-    }
-    return false
+    event.preventDefault();
   }
 
   const emailRegex = /^\S+@\S+\.\S+$/
   const nameRegex = /^[a-z ,.'-]+$/i
-  const usernameRegex = /^[a-z._-]+$/i
+  const usernameRegex = /^[a-z._0-9-]+$/i
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/
 
-  function validateForm() {
-    // Validate email
-    if (!emailRegex.test(email)) {
-      setError = true
-      setErrorMessages(errorMessages.concat(['Please enter a valid email.']))
+  function validateEmail() {
+    if (email.length == 0) {
+      return 'Email cannot be empty'
     }
-    
-    // Validate first and last name
+    if (!emailRegex.test(email)) {
+      return 'Please enter a valid email'
+    }
+  }
+
+  function validateFirstName() {
+    if (firstName.length == 0) {
+      return 'First name cannot be empty'
+    }
     if (!nameRegex.test(firstName)) {
-        setError = true
-        setErrorMessages(errorMessages.concat(['First name contains invalid characters.']))
+      return 'First name contains invalid characters'
+    }
+  }
+
+  function validateLastName() {
+    if (lastName.length == 0) {
+      return 'Last name cannot be empty'
     }
     if (!nameRegex.test(lastName)) {
-      setError = true
-      setErrorMessages(errorMessages.concat(['Last name contains invalid characters.']))
+      return 'Last name contains invalid characters'
     }
-    
-    // Validate username
-    if (!usernameRegex.test(username)) {
-      setError = true
-      setErrorMessages(errorMessages.concat(['Username contains invalid characters']))
-    }
+  }
 
-    // Validate password
+  function validateUsername() {
+    if (username.length === 0) {
+      return 'Username cannot be empty'
+    }
+    if (!usernameRegex.test(username)) {
+      return 'Username contains invalid characters'
+    }
+  }
+
+  function validatePassword() {
+    if (password.length === 0) {
+      return 'Password cannot be empty'
+    }
     if (!passwordRegex.test(password)) {
-      setError = true
-      setErrorMessages(errorMessages.concat(['Password must contain at least 8 characters, have 1 uppercase letter, ' + 
-        '1 lowercase letter, and 1 number.']))
+      return 'Password must contain at least 8 characters, 1 uppercase letter, ' + 
+        '1 lowercase letter, and 1 number'
+    }
+  }
+
+  function validateReEnterPassword() {
+    if (ReEnterPassword.length === 0) {
+      return 'Must re-enter password'
     }
     if (!(password === ReEnterPassword)) {
-      setError = true
-      setErrorMessages(errorMessages.concat(['Passwords do not match.']))
+      return 'Passwords do not match'
     }
   }
 
   return (
     <div className='createAccount'>
       <Form onSubmit={handleSubmit}>
-      <Form.Group className={error ? 'text-muted' : 'hidden'}>
-        <Form.Text>
-            { errorMessages.map((message, errorIndex) => (
-            <p key={errorIndex}>{message}</p>)) }
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className='formInput' controlId='email'>
+        <Form.Group className='formInput' controlId='email'>
+            <Form.Control
+              required
+              aria-label='Email'
+              size='md'
+              type='email'
+              placeholder='Email'
+              value={email}
+              onChange={(e) => {setEmail(e.target.value); setInitialEmail(false)}}
+            />
+            <Form.Text id='errorMessage' muted>
+              { !initialEmail ? validateEmail() : '' }
+            </Form.Text>
+        </Form.Group>
+        <Form.Group className='formInput' controlId='firstName'>
+            <Form.Control
+              required
+              aria-label='First Name'
+              size='md'
+              type='firstName'
+              placeholder='First Name'
+              value={firstName}
+              onChange={(e) => {setFirstName(e.target.value); setInitialFirstName(false)}}
+            />
+            <Form.Text id='errorMessage' muted>
+              { !initialFirstName ? validateFirstName() : '' }
+            </Form.Text>
+        </Form.Group>
+        <Form.Group className='formInput' controlId='lastName'>
           <Form.Control
-            size='md'
-            type='email'
-            placeholder='Email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-      </Form.Group>
-      <Form.Group className='formInput' controlId='firstName'>
-          <Form.Control
-            size='md'
-            type='firstName'
-            placeholder='First Name'
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-      </Form.Group>
-      <Form.Group className='formInput' controlId='lastName'>
-          <Form.Control
+            required
+            aria-label='Last Name'
             size='md'
             type='lastName'
             placeholder='Last Name'
             value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
+            onChange={(e) => {setLastName(e.target.value); setInitialLastName(false)}}
           />
+          <Form.Text id='errorMessage' muted>
+              { !initialLastName ? validateLastName() : '' }
+            </Form.Text>
         </Form.Group>
         <Form.Group className='formInput' controlId='username'>
           <Form.Control
+            required
+            aria-label='Username'
             size='md'
             type='username'
             placeholder='Username'
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) => {setUsername(e.target.value); setInitialUsername(false)}}
           />
+          <Form.Text id='errorMessage' muted>
+              { !initialUsername ? validateUsername() : '' }
+            </Form.Text>
         </Form.Group>
         <Form.Group className='formInput' controlId='password'>
           <Form.Control
+            required
+            aria-label='Password'
             size='md'
             type='password'
             placeholder='Password'
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {setPassword(e.target.value); setInitialPassword(false)}}
+            aria-describedby='passwordHelp'
           />
+          <Form.Text id='errorMessage' muted>
+              { !initialPassword ? validatePassword() : '' }
+            </Form.Text>
         </Form.Group>
         <Form.Group className='formInput' controlId='reEnterPassword'>
           <Form.Control
+            required
+            aria-label='Re-enter Password'
             size='md'
             type='password'
             placeholder='Re-enter Password'
             value={ReEnterPassword}
-            onChange={(e) => setReEnterPassword(e.target.value)}
+            onChange={(e) => {setReEnterPassword(e.target.value); setInitialReEnterPassword(false)}}
           />
+          <Form.Text id='errorMessage' muted>
+              { !initialReEnterPassword ? validateReEnterPassword() : '' }
+            </Form.Text>
         </Form.Group>
-        <Button className='submitButton' type='submit' variant='info' block disabled={emptyField}>
+        <Button className='submitButton' type='submit' variant='info' block>
           Create Account
         </Button>
       </Form>
