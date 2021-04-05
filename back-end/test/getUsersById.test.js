ed6efe8b3c8be3db6e2a9efb1ac7adb8661e9ea4
Jonathan Sussman <60412241+JonathanSussman1@users.noreply.github.com>
2021-04-04 22:12:08 -0400

    removing semicolons

--- back-end/test/getUsersById.test.js
+++ back-end/test/getUsersById.test.js
@@ -15,14 +15,14 @@ describe("Testing GET for /usersbyid API", () => {
     it("should return >1 user objects", () => { 
         return chai.request(app).get('/usersbyid').then((response) => { 
             //user 1
-            expect((response.body)[0]).to.have.property('username').that.is.a('string');
-            expect((response.body)[0]).to.have.property('followers').that.is.an('array');
-            expect((response.body)[0]).to.have.property('notificationSettings').that.is.an('object').with.deep.property('emailNotifications');
+            expect((response.body)[0]).to.have.property('username').that.is.a('string')
+            expect((response.body)[0]).to.have.property('followers').that.is.an('array')
+            expect((response.body)[0]).to.have.property('notificationSettings').that.is.an('object').with.deep.property('emailNotifications')
 
             //user 2
-            expect((response.body)[1]).to.have.property('username').that.is.a('string');
-            expect((response.body)[1]).to.have.property('followers').that.is.an('array');
-            expect((response.body)[1]).to.have.property('notificationSettings').that.is.an('object').with.deep.property('emailNotifications');
+            expect((response.body)[1]).to.have.property('username').that.is.a('string')
+            expect((response.body)[1]).to.have.property('followers').that.is.an('array')
+            expect((response.body)[1]).to.have.property('notificationSettings').that.is.an('object').with.deep.property('emailNotifications')
         })
     }).timeout(4000)
 })
