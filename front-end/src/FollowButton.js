f84cce23e45345e022ed58243f3901c964af78c3
olivia edell <oliviaedell@10-16-97-246.dynapool.wireless.nyu.edu>
2021-04-12 20:42:28 -0400

    add back import axios

--- front-end/src/FollowButton.js
+++ front-end/src/FollowButton.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react"
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
-
-import "./FollowButton.css"
-
+import axios from 'axios'
+import './FollowButton.css'
 
 const FollowButton = (props) => {
     let state = 'Follow'
@@ -12,43 +11,52 @@ const FollowButton = (props) => {
     /* text is a state variable that changes the text
   on the button depending on whether the active user 
   is already following the profile they are viewing */
-  let [text, setText] = useState(state)
+    let [text, setText] = useState(state)
 
-  // click event handler changes text state 
-  function follow() {
- if (props.signedIn) {
-      setText((prevText) => {
-        if (prevText === "Follow") {
-          return "Following"
+    // click event handler changes text state
+    function follow() {
+        if (props.signedIn) {
+            setText((prevText) => {
+                if (prevText === 'Follow') {
+                    return 'Following'
+                } else {
+                    return 'Follow'
+                }
+            })
+
+            // TODO: update props.currentUser.following and props.profileUserId's user .following in the database
+
+            const followData = {
+                signedInUserId: props.user._id,
+                followedUserId: props.profileUser._id,
+                follow:
+                    props.profileUser._id in props.user.following ? false : true
+            }
+
+            axios.post('http://localhost:4000/followuser', followData)
         } else {
-          return "Follow"
+            // show sign-in modal if a not-signed in user attempts to follow a user
+            props.setShowModal(true)
         }
-      })
-   
-   // TODO: update props.currentUser.following and props.profileUserId's user .following in the database
-       
-   const followData = {           
-     signedInUserId: props.user._id,                
-     followedUserId: props.profileUser._id,           
-     follow:
-        props.profileUser._id in props.user.following ? false : true
-   }
-
-            axios.post('http://localhost:4000/followuser', followData)     
     }
-    else {
-      // show sign-in modal if a not-signed in user attempts to follow a user
-      props.setShowModal(true)
-    }
-    
-  }
 
-/* component */
-  return (
-    <Button block size="sm" variant={(text === 'Follow') ? 'info' : 'outline-info'} id="followBtn" className={(text === 'Follow') ? 'followBtnNotFollowing' : 'followBtnFollowing'} onClick={follow}>
-      {text}
-    </Button>
-  );
-};
+    /* component */
+    return (
+        <Button
+            block
+            size="sm"
+            variant={text === 'Follow' ? 'info' : 'outline-info'}
+            id="followBtn"
+            className={
+                text === 'Follow'
+                    ? 'followBtnNotFollowing'
+                    : 'followBtnFollowing'
+            }
+            onClick={follow}
+        >
+            {text}
+        </Button>
+    )
+}
 
 export default FollowButton
