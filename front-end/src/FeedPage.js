4161df9931df9de158c775592ec15d6ce928c21e
Ashley Nussbaum <amn493@nyu.edu>
2021-04-19 23:11:07 -0400

    Fix feed bug on initial sign-in

--- front-end/src/FeedPage.js
+++ front-end/src/FeedPage.js
@@ -26,42 +26,45 @@ const Feed = (props) => {
 
     /* Pull in recipes from mockaroo using the GET route handler */
     useEffect(() => {
-        // Get a list of feed recipes
-        // userid is the logged in user so we can get their following
-        // timestamp is the current time to pull recipes from a certain timeframe, e.g. a week
+        if (props.user.username) {
+            // Get a list of feed recipes
+            // userid is the logged in user so we can get their following
+            // timestamp is the current time to pull recipes from a certain timeframe, e.g. a week
 
-        let followingArray = props.user.following
+            let followingArray = props.user.following
 
-        // Check the array length and if it's zero (e.g. user doesn't follow anyone), generate a custom error component
-        if (followingArray.length === 0) {
-            setReqError(true)
-            setErrMsg(
-                "You don't follow anyone. Check out the browse users page to find some tasty recipes!"
-            )
-        } else {
-            // Otherwise, go ahead and query the database
-            axios(
-                `http://localhost:4000/feedrecipes?${
-                    followingArray.length > 0
-                        ? followingArray.reduce(
-                              (acc, following) =>
-                                  acc + `&following=${following}`,
-                              `following=`
-                          )
-                        : `following=`
-                }&datemultiplier=${dateMultiplier}`
-            )
-                .then((response) => {
-                    setRecBoxRecipes(response.data)
-                })
-                .catch((err) => {
-                    console.log(err)
-                    setReqError(true)
-                    setRecBoxRecipes() // Returns empty, that way we can check in the RecipeList component if it's empty and return an error if so (e.g. "no recipes found")
-                })
-            setDateMultiplier(dateMultiplier + 1)
+            // Check the array length and if it's zero (e.g. user doesn't follow anyone), generate a custom error component
+            if (followingArray.length === 0) {
+                setReqError(true)
+                setErrMsg(
+                    "You don't follow anyone. Check out the browse users page to find some tasty recipes!"
+                )
+            } else {
+                // Otherwise, go ahead and query the database
+                axios(
+                    `http://localhost:4000/feedrecipes?${
+                        followingArray.length > 0
+                            ? followingArray.reduce(
+                                  (acc, following) =>
+                                      acc + `&following=${following}`,
+                                  `following=`
+                              )
+                            : `following=`
+                    }&datemultiplier=${dateMultiplier}`
+                )
+                    .then((response) => {
+                        setRecBoxRecipes(response.data)
+                    })
+                    .catch((err) => {
+                        console.error(err)
+                        setReqError(true)
+                        setRecBoxRecipes() // Returns empty, that way we can check in the RecipeList component if it's empty and return an error if so (e.g. "no recipes found")
+                    })
+                setDateMultiplier(dateMultiplier + 1)
+            }
         }
-    }, [props.user.following])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.user])
 
     // To be called when the reload button is pressed
     const loadMoreRecipes = () => {
@@ -88,28 +91,32 @@ const Feed = (props) => {
         setRecLoadedText(`Showing recipes from ${dateMultiplier * 2} weeks ago`)
     }
 
-    return !reqError ? (
-        <>
-            <div className="container">
-                <RecipeList
-                    size="large"
-                    recipes={recBoxRecipes}
-                    user={props.user}
-                />
-                <div className="recLoadedText">{recLoadedText}</div>
-                <Button
-                    block
-                    size="sm"
-                    variant="info"
-                    id="loadMoreRecipesBtn"
-                    onClick={loadMoreRecipes}
-                >
-                    Load Older Recipes
-                </Button>
-            </div>
-        </>
+    return props.user.username ? (
+        !reqError ? (
+            <>
+                <div className="container">
+                    <RecipeList
+                        size="large"
+                        recipes={recBoxRecipes}
+                        user={props.user}
+                    />
+                    <div className="recLoadedText">{recLoadedText}</div>
+                    <Button
+                        block
+                        size="sm"
+                        variant="info"
+                        id="loadMoreRecipesBtn"
+                        onClick={loadMoreRecipes}
+                    >
+                        Load Older Recipes
+                    </Button>
+                </div>
+            </>
+        ) : (
+            <ErrorComponent error={errMsg} />
+        )
     ) : (
-        <ErrorComponent error={errMsg} />
+        <></>
     )
 }
 
