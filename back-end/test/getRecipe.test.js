a063bb64e8ae6b631c040552274a34c059ac1e76
anvitayella <ay1313@nyu.edu>
2021-04-25 19:52:51 -0400

    fixed getRecipe.test.js

--- back-end/test/getRecipe.test.js
+++ back-end/test/getRecipe.test.js
@@ -11,37 +11,32 @@ const app = require('../app.js')
 
 describe('Testing route handler for GET /recipe ', () => {
     it('should return 200 OK status', () => {
-    return chai.request(app).get('/recipe?slug=foobar-eggs').then((response) => {
-        expect(response.status).to.equal(200)
-      })
+        return chai.request(app).get('/recipe?slug=bread-sandwich').then((response) => {
+            expect(response.status).to.equal(200)
+        })
     })
 
-  it('should return a recipe object with correct fields', () => {
-    return chai.request(app).get('/recipe?slug=foobar-eggs').then((response) => {
-        expect((response.body)[0]).to.have.property('name')
-        expect((response.body)[0]).to.have.property('imagePath')
-        expect((response.body)[0]).to.have.property('tags')
-        expect((response.body)[0]).to.have.property('caption')
-        expect((response.body)[0]).to.have.property('ingredients')
-        expect((response.body)[0]).to.have.property('instructions')
-        expect((response.body)[0]).to.have.property('likes')
-        expect((response.body)[0]).to.have.property('createdAt')
-        expect((response.body)[0]).to.have.property('slug')
-        expect((response.body)[0]).to.have.property('id')
+    it('should return a recipe object with the correct fields', () => {
+        return chai.request(app).get('/recipe?slug=bread-sandwich').then((response) => {
+            expect(response.body).to.have.property('_id')
+            expect(response.body).to.have.property('tags')
+            expect(response.body).to.have.property('ingredients')
+            expect(response.body).to.have.property('instructions')
+            expect(response.body).to.have.property('slug')
+            expect(response.body).to.have.property('user')
+            expect(response.body).to.have.property('name')
+            expect(response.body).to.have.property('imagePath')
+            expect(response.body).to.have.property('caption')
+            expect(response.body).to.have.property('likes')
+            expect(response.body).to.have.property('createdAt')
+        })
     }).timeout(8000)
-  })
 
     it('should return a recipe object with the correct field types', () => {
-        return chai.request(app).get('/recipe?slug=foobar-eggs').then((response) => {
-            expect((response.body)[0]).to.have.property('name').that.is.a('string')
-            expect((response.body)[0]).to.have.property('ingredients').that.is.an('array')
-            expect((response.body)[0]).to.have.property('id').that.is.a('int')
+        return chai.request(app).get('/recipe?slug=bread-sandwich').then((response) => {
+            expect(response.body).to.have.property('name').that.is.a('string')
+            expect(response.body).to.have.property('ingredients').that.is.an('array')
+            expect(response.body).to.have.property('imagePath').that.is.a('string')
         })
     }).timeout(8000)
-
-    it('should only return one recipe object ', () => {
-        return chai.request(app).get('/recipe?slug=foobar-eggs').then((response) => {
-            expect(response.body.length).to.equal(1)
-        })
-    }).timeout(3000)
 })
