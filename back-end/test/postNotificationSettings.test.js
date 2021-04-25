62b33ef165158c91dcf21084f4fedf39b9750bf4
Jonathan Susman <jas1611@nyu.edu>
2021-04-25 17:38:06 -0400

    fixing postNotificationSettings.test.js

--- back-end/test/postNotificationSettings.test.js
+++ back-end/test/postNotificationSettings.test.js
@@ -1,81 +1,142 @@
-const chai = require("chai")
-const chaiHTTP = require("chai-http")
-const expect = chai.expect
-const faker = require('faker');
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable consistent-return */
+const chai = require('chai')
+
+const chaiHTTP = require('chai-http')
+require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
+
+const mongoose = require('mongoose')
+require('../db.js')
+const JWT = require('jsonwebtoken')
+
+const User = mongoose.model('User')
+
+const { expect } = chai
+// const faker = require('faker')
 
 chai.use(chaiHTTP)
 
-const app = require("../app.js")
+const app = require('../app.js')
 
-describe("Testing POST to /blockusers API", () => { 
-    it("should return 200 OK status", () => { 
+describe('Testing POST to /notificationsettings API', () => {
+    it('should return 200 OK status', () => {
+        // dummy user 6078f5faaa84a40750ad4518
+        User.findOne(
+            { _id: mongoose.Types.ObjectId('6078f5faaa84a40750ad4518') },
+            (user) => {
+                if (user) {
+                    const {
+                        emailNotifications
+                    } = user.notificationSettings.emailNotifications
+                    const { likes } = user.notificationSettings.likes
+                    const { comments } = user.notificationSettings.comments
+                    const { follows } = user.notificationSettings.follows
+                    const { id } = user._id
+                    return chai
+                        .request(app)
+                        .post('/notificationsettings')
+                        .send({
+                            emailNotifications: emailNotifications,
+                            likes: likes,
+                            comments: comments,
+                            follows: follows,
+                            userID: id
+                        })
+                        .then((response) => {
+                            expect(response.status).to.equal(200)
+                        })
+                }
+            }
+        )
+    })
+    it('should return an object with correct field names', () => {
+        // dummy user
+        User.findOne(
+            { _id: mongoose.Types.ObjectId('6078f5faaa84a40750ad4518') },
+            (user) => {
+                if (user) {
+                    const {
+                        emailNotifications
+                    } = user.notificationSettings.emailNotifications
+                    const { likes } = user.notificationSettings.likes
+                    const { comments } = user.notificationSettings.comments
+                    const { follows } = user.notificationSettings.follows
+                    const { id } = user._id
+                    return chai
+                        .request(app)
+                        .post('/notificationsettings')
+                        .send({
+                            emailNotifications: emailNotifications,
+                            likes: likes,
+                            comments: comments,
+                            follows: follows,
+                            userID: id
+                        })
+                        .then((response) => {
+                            expect(response.body).to.have.property(
+                                'notificationSettings'
+                            )
+                            expect(
+                                response.body.notificationSettings
+                            ).to.have.property('emailNotifications')
+                            expect(
+                                response.body.notificationSettings
+                            ).to.have.property('likes')
+                            expect(
+                                response.body.notificationSettings
+                            ).to.have.property('comments')
+                            expect(
+                                response.body.notificationSettings
+                            ).to.have.property('follows')
+                        })
+                }
+            }
+        )
+    })
 
-        //dummy data
-        let email = true
-        let likes = false
-        let comments = true
-        let followers = false
-        let id = 21
-
-       return chai.request(app)
-        .post('/notificationsettings')
-        .send({email: email, 
-            likes: likes, 
-            comments: comments,
-            followers: followers,
-            id: id
-            }).then((response) => { 
-                expect(response.status).to.equal(200)
-            })
-    }).timeout(4000)
-
-    it("should return an object with correct field names", () => { 
-
-        //dummy data
-        let email = true
-        let likes = false
-        let comments = true
-        let followers = false
-        let id = 21
-
-        return chai.request(app)
-         .post('/notificationsettings')
-         .send({email: email, 
-            likes: likes, 
-            comments: comments,
-            followers: followers,
-            id: id
-            }).then((response) => { 
-                expect(response.body).to.have.property('email')
-                expect(response.body).to.have.property('likes')
-                expect(response.body).to.have.property('comments')
-                expect(response.body).to.have.property('followers')
-                expect(response.body).to.have.property('id')
-             })
-     }).timeout(8000)
-
-     it("should return an object with correct field data types", () => { 
-
-        //dummy data
-        let email = true
-        let likes = false
-        let comments = true
-        let followers = false
-        let id = 21
-
-        return chai.request(app)
-         .post('/blockuser')
-         .send({email: email, 
-            likes: likes, 
-            comments: comments,
-            followers: followers,
-            id: id
-            }).then((response) => { 
-                expect(response.body.email).to.be.a('boolean')
-                expect(response.body.likes).to.be.a('boolean')
-                expect(response.body.comments).to.be.a('boolean')
-                expect(response.body.followers).to.be.a('boolean')
-                expect(response.body.id).to.be.an('int')
-             })
-     }).timeout(8000)
+    it('should return an object with correct field data types', () => {
+        // dummy user
+        User.findOne(
+            { _id: mongoose.Types.ObjectId('6078f5faaa84a40750ad4518') },
+            (user) => {
+                if (user) {
+                    const {
+                        emailNotifications
+                    } = user.notificationSettings.emailNotifications
+                    const { likes } = user.notificationSettings.likes
+                    const { comments } = user.notificationSettings.comments
+                    const { follows } = user.notificationSettings.follows
+                    const { id } = user._id
+                    return chai
+                        .request(app)
+                        .post('/notificationsettings')
+                        .send({
+                            emailNotifications: emailNotifications,
+                            likes: likes,
+                            comments: comments,
+                            follows: follows,
+                            userID: id
+                        })
+                        .then((response) => {
+                            expect(response.body.notificationSettings).to.be.an(
+                                'object'
+                            )
+                            expect(
+                                response.body.notificationSettings
+                                    .emailNotifications
+                            ).to.be.a('boolean')
+                            expect(
+                                response.body.notificationSettings.likes
+                            ).to.be.a('boolean')
+                            expect(
+                                response.body.notificationSettings.comments
+                            ).to.be.a('boolean')
+                            expect(
+                                response.body.notificationSettings.follows
+                            ).to.be.a('boolean')
+                        })
+                }
+            }
+        )
+    })
 })
