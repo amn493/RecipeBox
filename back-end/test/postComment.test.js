e91325915b42a4b830dc05766a6021d0587bb243
Mark <markbacon78@gmail.com>
2021-05-02 09:47:53 -0400

    fix postComment.js

--- back-end/test/postComment.test.js
+++ back-end/test/postComment.test.js
@@ -8,15 +8,22 @@ const expect = chai.expect
 chai.use(chaiHTTP)
 require('dotenv').config({ silent: true })
 
+const ObjectId = require('mongodb').ObjectId
+const mongoose = require('mongoose')
+require('../db.js')
+
+const Comment = mongoose.model('Comment')
+
 // Import our app.js where the route handlers are
 const app = require('../app.js')
 // Create a mock comment
-const mockRecId = '60822725cc7a916181964c79'
+const mockRecId = '6082ea082ab8214b6c4bad4e'
 const mockUserId = '606fb5fb05f0cb9a1c8eb2cb'
 let mockComment = {
     recipe: mockRecId,
     user: mockUserId,
-    comment: 'Love this recipe!'
+    comment:
+        'Testing postComment.test.js! If you see this, delete it and fix the unit test.'
 }
 
 // If Mockaroo is down, change whatever is in your route to "res.send('Text')" and comment out the axios call to make sure the test runs.
@@ -30,11 +37,17 @@ describe('Testing POST for /comment API', () => {
             .then((response) => {
                 // Have chai request app.js and then call the url to that route handler
                 expect(response.status).to.equal(200)
+
+                Comment.deleteOne({
+                    _id: ObjectId(response.body._id)
+                }).then(() => {
+                    // If you remove the .then(), the comment doesn't get deleted. Not sure why, but that's why this is here
+                })
             })
     })
 
-    it('should return the comment that was added', () => {
-        let resultResponse = chai
+    it('should return the same comment that was added', () => {
+        return chai
             .request(app)
             .post('/comment')
             .send(mockComment)
@@ -45,8 +58,15 @@ describe('Testing POST for /comment API', () => {
                     'comment',
                     'createdAt'
                 )
-            })
 
-        return resultResponse
+                expect(response.body.recipe).to.equal(mockRecId)
+                expect(response.body.user).to.equal(mockUserId)
+                expect(response.body.comment).to.equal(mockComment.comment)
+                Comment.deleteOne({
+                    _id: ObjectId(response.body._id)
+                }).then(() => {
+                    // If you remove the .then(), the comment doesn't get deleted. Not sure why, but that's why this is here
+                })
+            })
     })
 })
