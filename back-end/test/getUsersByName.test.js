2da3fd3e9ce68d81fab8f3e79de91a8d09fa7a47
Mark <markbacon78@gmail.com>
2021-05-02 10:04:10 -0400

    update getusersbyname test

--- back-end/test/getUsersByName.test.js
+++ back-end/test/getUsersByName.test.js
@@ -23,54 +23,83 @@ const app = require('../app.js')
 // Group of tests
 describe('Testing route handler for GET /usersbyname ', () => {
     // get 3 users "randomly"
-    let userNames = []
-    let userObjs = []
+    let pulledUser = {}
     before(async () => {
-        await User.find({})
-            .limit(3)
-            .then((user) => {
-                user.forEach((singleUser) => {
-                    userNames.push(singleUser.username)
-                    userObjs.push(singleUser)
-                })
-            })
+        await User.findOne({}).then((user) => {
+            pulledUser = user
+        })
     })
+
     it('should return 200 OK status ', () => {
         return chai
             .request(app)
-            .get(`/usersbyname?name=${userNames[0]}`)
+            .get(`/usersbyname?name=${pulledUser.username}`)
             .then((response) => {
                 expect(response.status).to.equal(200)
             })
     }).timeout(3000)
 
-    it('should return a valid user object', () => {
+    // The following tests simply search for the found user's firstName, lastName, then username
+    it('should return a user object whose firstName matches that which was pulled from database', () => {
         return chai
             .request(app)
-            .get(`/usersbyname?name=${userNames[0]}`)
+            .get(`/usersbyname?name=${pulledUser.firstName}`)
             .then((response) => {
-                //user 1
-                expect(response.body[0])
-                    .to.have.property('username')
-                    .that.is.a('string')
-                expect(response.body[0])
-                    .to.have.property('followers')
-                    .that.is.an('array')
-                expect(response.body[0])
-                    .to.have.property('notificationSettings')
-                    .that.is.an('object')
-                    .with.deep.property('emailNotifications')
+                // list should be of at least size one, so check the first item
+                expect(response.body[0].firstName).to.equal(
+                    pulledUser.firstName
+                )
             })
     })
 
-    it('should return user object that was called', () => {
-        // they're reverse order because I pushed the array earlier vs appending
+    it('should return a user object whose lastName matches that which was pulled from database', () => {
         return chai
             .request(app)
-            .get(`/usersbyname?name=${userNames[0]}`)
+            .get(`/usersbyname?name=${pulledUser.firstName}`)
             .then((response) => {
-                //user 1
-                expect(response.body[0].username).to.equal(userObjs[0].username)
+                // list should be of at least size one, so check the first item
+                expect(response.body[0].lastName).to.equal(pulledUser.lastName)
+            })
+    })
+
+    it('should return a user object whose username matches that which was pulled from database', () => {
+        return chai
+            .request(app)
+            .get(`/usersbyname?name=${pulledUser.username}`)
+            .then((response) => {
+                // list should be of at least size one, so check the first item
+                expect(response.body[0].username).to.equal(pulledUser.username)
+            })
+    })
+
+    it('should return an array where each item has at least one valid field', () => {
+        return chai
+            .request(app)
+            .get(`/usersbyname?name=${pulledUser.username}`)
+            .then((response) => {
+                if (response.body.length > 1) {
+                    response.body.forEach((user) => {
+                        expect(user.username.toLowerCase()).to.equal(
+                            pulledUser.username.toLowerCase()
+                        ) ||
+                            expect(user.firstName.toLowerCase()).to.equal(
+                                pulledUser.username.toLowerCase()
+                            ) ||
+                            expect(user.lastName.toLowerCase()).to.equal(
+                                pulledUser.username.toLowerCase()
+                            )
+                    })
+                } else {
+                    expect(response.body[0].username.toLowerCase()).to.equal(
+                        pulledUser.username.toLowerCase()
+                    ) ||
+                        expect(
+                            response.body[0].firstName.toLowerCase()
+                        ).to.equal(pulledUser.username.toLowerCase()) ||
+                        expect(
+                            response.body[0].lastName.toLowerCase()
+                        ).to.equal(pulledUser.username.toLowerCase())
+                }
             })
     })
 })
