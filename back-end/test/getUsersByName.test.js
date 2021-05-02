8d1c68dc19ec9c306e506f020caa8bc7020f671d
Ashley Nussbaum <amn493@nyu.edu>
2021-05-02 13:15:50 -0400

    Check that all users in responses have a matching field

--- back-end/test/getUsersByName.test.js
+++ back-end/test/getUsersByName.test.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 // Test expected output when given valid input
-// Test expected output when given invalid input
 
 const chai = require('chai')
 const chaiHTTP = require('chai-http')
@@ -17,12 +16,8 @@ const User = mongoose.model('User')
 
 const app = require('../app.js')
 
-// If Mockaroo is down, change route to "res.send('Text')" and
-// comment out the axios to make sure the test runs.
-
 // Group of tests
 describe('Testing route handler for GET /usersbyname ', () => {
-    // get 3 users "randomly"
     let pulledUser = {}
     before(async () => {
         await User.findOne({}).then((user) => {
@@ -39,67 +34,48 @@ describe('Testing route handler for GET /usersbyname ', () => {
             })
     }).timeout(3000)
 
-    // The following tests simply search for the found user's firstName, lastName, then username
-    it('should return a user object whose firstName matches that which was pulled from database', () => {
-        return chai
-            .request(app)
-            .get(`/usersbyname?name=${pulledUser.firstName}`)
-            .then((response) => {
-                // list should be of at least size one, so check the first item
-                expect(response.body[0].firstName).to.equal(
-                    pulledUser.firstName
-                )
-            })
-    })
-
-    it('should return a user object whose lastName matches that which was pulled from database', () => {
-        return chai
-            .request(app)
-            .get(`/usersbyname?name=${pulledUser.firstName}`)
-            .then((response) => {
-                // list should be of at least size one, so check the first item
-                expect(response.body[0].lastName).to.equal(pulledUser.lastName)
-            })
-    })
-
-    it('should return a user object whose username matches that which was pulled from database', () => {
+    it('should return an array where each user has at least one matching field (username)', () => {
         return chai
             .request(app)
             .get(`/usersbyname?name=${pulledUser.username}`)
             .then((response) => {
-                // list should be of at least size one, so check the first item
-                expect(response.body[0].username).to.equal(pulledUser.username)
+                response.body.forEach((user) => {
+                    expect([
+                        user.username.toLowerCase(),
+                        user.firstName.toLowerCase(),
+                        user.lastName.toLowerCase()
+                    ]).to.include(pulledUser.username.toLowerCase())
+                })
             })
     })
 
-    it('should return an array where each item has at least one valid field', () => {
+    it('should return an array where each user has at least one matching field (first name)', () => {
         return chai
             .request(app)
-            .get(`/usersbyname?name=${pulledUser.username}`)
+            .get(`/usersbyname?name=${pulledUser.firstName}`)
             .then((response) => {
-                if (response.body.length > 1) {
-                    response.body.forEach((user) => {
-                        expect(user.username.toLowerCase()).to.equal(
-                            pulledUser.username.toLowerCase()
-                        ) ||
-                            expect(user.firstName.toLowerCase()).to.equal(
-                                pulledUser.username.toLowerCase()
-                            ) ||
-                            expect(user.lastName.toLowerCase()).to.equal(
-                                pulledUser.username.toLowerCase()
-                            )
-                    })
-                } else {
-                    expect(response.body[0].username.toLowerCase()).to.equal(
-                        pulledUser.username.toLowerCase()
-                    ) ||
-                        expect(
-                            response.body[0].firstName.toLowerCase()
-                        ).to.equal(pulledUser.username.toLowerCase()) ||
-                        expect(
-                            response.body[0].lastName.toLowerCase()
-                        ).to.equal(pulledUser.username.toLowerCase())
-                }
+                response.body.forEach((user) => {
+                    expect([
+                        user.username.toLowerCase(),
+                        user.firstName.toLowerCase(),
+                        user.lastName.toLowerCase()
+                    ]).to.include(pulledUser.firstName.toLowerCase())
+                })
+            })
+    })
+
+    it('should return an array where each user has at least one matching field (last name)', () => {
+        return chai
+            .request(app)
+            .get(`/usersbyname?name=${pulledUser.lastName}`)
+            .then((response) => {
+                response.body.forEach((user) => {
+                    expect([
+                        user.username.toLowerCase(),
+                        user.firstName.toLowerCase(),
+                        user.lastName.toLowerCase()
+                    ]).to.include(pulledUser.lastName.toLowerCase())
+                })
             })
     })
 })
