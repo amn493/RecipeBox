c4c6d31ef1be628cfcbc9f0b7d6f571fd4e0d79e
anvitayella <ay1313@nyu.edu>
2021-05-03 13:31:54 -0400

    unit tests for deleteRecipe

---
+++ back-end/test/postDeleteRecipe.test.js
@@ -0,0 +1,65 @@
+const chai = require('chai')
+const chaiHTTP = require('chai-http')
+const { response } = require('../app.js')
+
+const { expect } = chai
+
+// Chai will run the test on a port not being used. Avoids using active server for tests.
+chai.use(chaiHTTP)
+
+// Import our app.js where the route handlers are
+const app = require('../app.js')
+const fs = require('fs')
+
+require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
+const mongoose = require('mongoose')
+require('../db.js')
+// eslint-disable-next-line no-unused-vars
+const JWT = require('jsonwebtoken')
+const Recipe = mongoose.model('Recipe')
+const User = mongoose.model('User')
+
+
+describe('Testing route handler for POST /newrecipe ', () => {
+    let userID = ''
+    let name = 'oatmeal'
+    let tags = JSON.stringify(['breakfast,oats,vegan'])
+    let caption = 'The best morning oatmeal!'
+    let ingredients = JSON.stringify(['rolled oats,banana,blueberries,chia seeds'])
+    let instructions = JSON.stringify(['boil oats in water,add toppings,enjoy!'])
+    let pinned = 'false'
+    let id = ''
+    let slug = ''
+
+    // Pull in a "random" user
+    before(async () => {
+        await User.findOne({}).then((user) => {
+            userID = user._id.toString()
+        })
+
+        await chai.request(app)
+        .post('/newrecipe')
+        .set('content-type', 'multipart/form-data')
+        .field('userID', userID)
+        .field('name', name)
+        .attach('recipeimage', fs.readFileSync('./test/image.png'), 'image.png')
+        .field('tags', tags)
+        .field('caption', caption)
+        .field('ingredients', ingredients)
+        .field('instructions', instructions)
+        .field('pinned', pinned)
+        .then((response) => {
+            id = response.body._id.toString()
+            slug = response.body.slug
+        })
+    })
+
+    it('should return 200 OK status', () => {
+        return chai.request(app)
+        .post('/deleterecipe')
+        .send({id})
+        .then((response) => {
+            expect(response.status).to.equal(200)
+        })
+    }).timeout(8000)
+})
