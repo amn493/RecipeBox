f09ed1e04a2ad08236a27bf2a6e739f3c482e591
olivia edell <47330607+oliviaedell@users.noreply.github.com>
2021-05-04 23:58:17 -0400

    add unlike test

--- back-end/test/postLikeRecipe.test.js
+++ back-end/test/postLikeRecipe.test.js
@@ -20,8 +20,7 @@ chai.use(chaiHTTP)
 
 const app = require('../app.js')
 
-describe('Testing POST to /likerecipe API', () => {
-    const like = true
+describe('Testing POST to /likerecipe API (like)', () => {
     let recipeID = ''
     let userID = ''
     let likes
@@ -39,31 +38,19 @@ describe('Testing POST to /likerecipe API', () => {
         })
     })
     afterEach(async () => {
-        await Recipe.findOne({}).then((recipe) => {
-            if (recipe) {
-                recipeID = recipe._id
-                likes = recipe.likes
-            }
-        })
-        await User.findOne({ liked: { $nin: recipeID } }).then((user) => {
-            if (user) {
-                userID = user._id
-            }
-
-            return chai.request(app).post('/likerecipe').send({
-                like: false,
-                recipeID: recipeID,
-                userID: userID
-            })
-        })
         console.log('\x1b[2m', '...unlike test...')
+        chai.request(app).post('/likerecipe').send({
+            like: false,
+            recipeID: recipeID,
+            userID: userID
+        })
     })
     it('should return 200 OK status', () =>
         chai
             .request(app)
             .post('/likerecipe')
             .send({
-                like: like,
+                like: true,
                 recipeID: recipeID,
                 userID: userID
             })
@@ -75,7 +62,7 @@ describe('Testing POST to /likerecipe API', () => {
             .request(app)
             .post('/likerecipe')
             .send({
-                like: like,
+                like: true,
                 recipeID: recipeID,
                 userID: userID
             })
@@ -90,19 +77,19 @@ describe('Testing POST to /likerecipe API', () => {
             .request(app)
             .post('/likerecipe')
             .send({
-                like: like,
+                like: true,
                 recipeID: recipeID,
                 userID: userID
             })
             .then((response) => {
-                expect(response.body.user.liked).to.not.include(recipeID)
+                expect(response.body.user.liked).to.include(recipeID.toString())
             }))
     it('should return recipe with correct field names', () =>
         chai
             .request(app)
             .post('/likerecipe')
             .send({
-                like: like,
+                like: true,
                 userID: userID,
                 recipeID: recipeID
             })
@@ -114,11 +101,103 @@ describe('Testing POST to /likerecipe API', () => {
             .request(app)
             .post('/likerecipe')
             .send({
-                like: like,
+                like: true,
                 recipeID: recipeID,
                 userID: userID
             })
             .then((response) => {
                 expect(response.body.recipe.likes).to.equal(likes + 1)
+            }))
+})
+
+describe('Testing POST to /likerecipe API (unlike)', () => {
+    let recipeID = ''
+    let userID = ''
+    let likes
+    beforeEach(async () => {
+        await Recipe.findOne({}).then((recipe) => {
+            if (recipe) {
+                recipeID = recipe._id
+                likes = recipe.likes
+            }
+        })
+        await User.findOne({ liked: { $in: recipeID } }).then((user) => {
+            if (user) {
+                userID = user._id
+            }
+        })
+    })
+    afterEach(async () => {
+        console.log('\x1b[2m', '...like test...')
+        chai.request(app).post('/likerecipe').send({
+            like: true,
+            recipeID: recipeID,
+            userID: userID
+        })
+    })
+    it('should return 200 OK status', () =>
+        chai
+            .request(app)
+            .post('/likerecipe')
+            .send({
+                like: false,
+                recipeID: recipeID,
+                userID: userID
+            })
+            .then((response) => {
+                expect(response.status).to.equal(200)
+            }))
+    it('should return user with correct field names', () =>
+        chai
+            .request(app)
+            .post('/likerecipe')
+            .send({
+                like: false,
+                recipeID: recipeID,
+                userID: userID
+            })
+            .then((response) => {
+                // eslint-disable-next-line no-unused-expressions
+                expect(response.body.user)
+                    .to.have.property('liked')
+                    .that.is.an('array').that.is.not.empty
+            }))
+    it('should return user with correct field data', () =>
+        chai
+            .request(app)
+            .post('/likerecipe')
+            .send({
+                like: false,
+                recipeID: recipeID,
+                userID: userID
+            })
+            .then((response) => {
+                expect(response.body.user.liked).to.not.include(
+                    recipeID.toString
+                )
+            }))
+    it('should return recipe with correct field names', () =>
+        chai
+            .request(app)
+            .post('/likerecipe')
+            .send({
+                like: false,
+                userID: userID,
+                recipeID: recipeID
+            })
+            .then((response) => {
+                expect(response.body.recipe).to.have.property('likes')
+            }))
+    it('should return recipe with correct field data', () =>
+        chai
+            .request(app)
+            .post('/likerecipe')
+            .send({
+                like: false,
+                recipeID: recipeID,
+                userID: userID
+            })
+            .then((response) => {
+                expect(response.body.recipe.likes).to.equal(likes - 1)
             }))
 })
