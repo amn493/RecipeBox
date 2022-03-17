7898bab6fae811c79fc81b09d37836a31baceb0e
Dendrobyte <markbacon78@gmail.com>
2022-03-17 10:35:34 -0700

    Use dynamic port assigned by heroku

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,6 @@
 const server = require('./app') // load up the web server
 
-const port = 4000 // the port to listen to for incoming requests
+const port = process.env.PORT || 4000 // the port to listen to for incoming requests
 // call express's listen function to start listening to the port
 const listener = server.listen(port, () => {
     // eslint-disable-next-line no-console
